11063bb2d74b273df1d9e60eeb1fb885986ad777
242209 <242209@edu.p.lodz.pl>
2023-03-03 18:58:49 +0100

    getting cell works for 6x6 -> 3x3 table

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,20 @@
 package org.example;
 
 import java.text.MessageFormat;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        Greeter greet = new Greeter(new MessageFormatter());
-        System.out.println(greet.greet("a"));
+//        Greeter greet = new Greeter(new MessageFormatter());
+//        System.out.println(greet.greet("a"));
 
+        SudokuBoard board1 = new SudokuBoard(6);
+        board1.fill_board();
+        board1.print_out2d(board1.board);
+
+        int[][] cell = board1.get_cell(1);
+        board1.print_out2d(cell);
     }
 }
 
@@ -31,3 +38,82 @@ class MessageFormatter{
     }
 }
 
+
+class SudokuBoard{
+    //cells inside sudoku are numbered
+    // 0 1 2
+    // 3 4 5
+    // 6 7 8
+
+    //////////////////
+
+    // 0 1
+    // 2 3
+
+    int[][] board;
+    int size;
+    int cell_size;
+    int cells_per_row;
+
+
+    SudokuBoard(int size){
+        this.size = size;
+        this.cell_size = 3;
+        this.cells_per_row = (int) size/cell_size;
+        board = new int[size][size];
+    }
+
+    void fill_board(){
+
+        for(int y =0; y<size; y++){
+            for(int x=0; x<size; x++){
+                board[y][x] = 0;
+            }
+
+        }
+
+        board[0][0] = 1;
+        board[5][0] = 4;
+        board[0][5] = 7;
+        board[5][5] = 9;
+    }
+
+    void print_out2d(int[][] board2d){
+        for(int[] row: board2d){
+            for(int item: row){
+                System.out.print(item);
+            }
+            System.out.print("\n");
+        }
+    }
+
+    int[][] get_cell(int cell_index){
+        int y = (int) cell_index/cells_per_row;
+        //System.out.print(y);
+        int x = cell_index % cells_per_row;
+        //System.out.print(x);
+
+        int[][] slice = new int[cell_size][cell_size];
+
+        for(int i =(y*cell_size); i<(y*cell_size)+cell_size;i++){
+            slice[i-(y*cell_size)] = Arrays.copyOfRange(board[i],x*cell_size,(x*cell_size)+cell_size );
+        }
+
+        // 0 -> 0:2 | 0:2 0 0
+        // 1 -> 0:2 | 2:4 1 0
+        // 2 -> 2:4 | 0:2 0 1
+        // 3 -> 2:4 | 2:4 1 1
+
+
+        return slice;
+
+
+
+    };
+
+
+
+
+
+
+}
--- src/test/java/org/example/SudokuBoardTest.java
+++ src/test/java/org/example/SudokuBoardTest.java
@@ -1,6 +1,19 @@
 package org.example;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class SudokuBoardTest {
-  
+    SudokuBoard test_board = new SudokuBoard(4);
+
+    @Test
+    void constructor(){
+        assertEquals(test_board.board.length,4);
+        assertEquals(test_board.board[0].length,4);
+
+
+
+    }
+
 }
